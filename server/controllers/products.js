3731908d7c121b271bb063647742e5b4aa060465
Adam McKenna <adam-mckenna@outlook.com>
2018-06-27 14:39:48 +0100

    CRUD operations for Product model

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -17,4 +17,69 @@ module.exports = {
       .then(product => res.status(201).send(product))
       .catch(error => res.status(400).send(error));
   },
+  
+  list(req, res) {
+    return Product
+      .all()
+      .then(products => res.status(200).send(products))
+      .catch(error => res.status(400).send(error));
+  },
+
+  retrieve(req, res) {
+    return Product
+      .findById(req.params.productId)
+      .then(product => {
+        if (!product) {
+          return res.status(404).send({
+            message: 'Product Not Found',
+          });
+        }
+        return res.status(200).send(product);
+      })
+      .catch(error => res.status(400).send(error));
+  },
+
+  update(req, res) {
+    return Product
+      .findById(req.params.productId)
+      .then(product => {
+        if (!product) {
+          return res.status(404).send({
+            message: 'Product Not Found',
+          });
+        }
+        return product
+          .update({
+            title: req.body.title || product.title,
+            adult_price: req.body.adult_price || product.adult_price,
+            child_price: req.body.child_price || product.child_price,
+            description: req.body.description || product.excerpt,
+            excerpt: req.body.excerpt || product.excerpt,
+            itinerary: req.body.itinerary || product.itinerary,
+            time: req.body.time || product.time,
+            image_url: req.body.image_url || product.image_url,
+            published: req.body.published || product.published,
+          })
+          .then(() => res.status(200).send(product)) 
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(400).send(error));
+  },
+
+  destroy(req, res) {
+    return Product
+      .findById(req.params.productId)
+      .then(product => {
+        if (!product) {
+          return res.status(400).send({
+            message: 'Product Not Found',
+          });
+        }
+        return product
+          .destroy()
+          .then(() => res.status(204).send())
+          .catch(error => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error));
+  },
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,8 @@ module.exports = (app) => {
   }));
 
   app.post('/api/products', productsController.create);
+  app.get('/api/products', productsController.list);
+  app.get('/api/products/:productId', productsController.retrieve);
+  app.put('/api/products/:productId', productsController.update);
+  app.delete('/api/products/:productId', productsController.destroy);
 };
