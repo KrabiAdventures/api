eceed83e99f94e5b307258c2310b1d165a8d49d5
Cal Whitehouse <cazwhitehouse@hotmail.com>
2018-07-24 13:16:48 +0100

    Unsure of any changes - needed to be commited before pull

---
+++ server/models/category.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    featured: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+    },
+  });
+
+  Category.associate = (models) => {
+    //
+  };
+
+  return Category;
+};
